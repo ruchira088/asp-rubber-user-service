65828544dac5ffaec6e9bc1178e7cd5eb336788f
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-04-02 11:45:14 +1000

    Refactoring

--- src/main/scala/com/ruchij/web/routes/AdminRoute.scala
+++ src/main/scala/com/ruchij/web/routes/AdminRoute.scala
@@ -3,10 +3,13 @@ package com.ruchij.web.routes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import com.ruchij.models.Administrator
 import com.ruchij.services.AdminService
 import com.ruchij.web.requests.CreateAdminRequest
 import com.ruchij.web.requests.validations.RequestBodyDirective._
-import com.ruchij.web.utils.JsonWriters._
+import com.ruchij.web.utils.JsonFormats._
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonWriter
 
 object AdminRoute
 {
@@ -21,4 +24,7 @@ object AdminRoute
         }
       }
     }
+
+  implicit def adminWriter: RootJsonWriter[Administrator] =
+    (admin: Administrator) => jsonFormat7(Administrator.apply).write(admin.sanitize)
 }
